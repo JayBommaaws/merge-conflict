460a76d88759a8b442248d3a3431311fc7b3a6d4
Ubuntu <ubuntu@ip-172-30-4-185.ec2.internal>
2024-10-02 07:20:34 +0000

    merge conflict resolved manually

--- main.c
+++ main.c
@@ -4,8 +4,11 @@ Initial Function()
 {
   //Sample Code
 }
+<<<<<<< HEAD
 This below code is added from main.c is a security patch
 
+=======
+This code is added to feature 2 as a security patch feature2
 Initial Security()
 {
  //Sample Code
